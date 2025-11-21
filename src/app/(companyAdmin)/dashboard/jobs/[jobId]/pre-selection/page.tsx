791b97294dfaf1e353639033973100185bb4745f
hasbi firdaus fauzun <hasbi.fauzun@gmail.com>
2025-11-21 13:09:02 +0700

    fix: resolve merge conflicts for job dashboard update

--- src/app/(companyAdmin)/dashboard/jobs/[jobId]/pre-selection/page.tsx
+++ src/app/(companyAdmin)/dashboard/jobs/[jobId]/pre-selection/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import { useParams } from "next/navigation";
-<<<<<<< HEAD
-import TestForm from "@/components/pre-selection/TestForm";
-=======
->>>>>>> 0166d84 (feat(frontend): update admin dashboard, job pages, interviews & pre-selection modules)
 import PreselectionTestEdit from "@/components/pre-selection-test/PreselectionTestEdit";
 
 const CreatePreSelectionTestPage = () => {
